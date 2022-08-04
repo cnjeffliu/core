5d6e36a563f8f92434863c3e5240bd371ddc6a4c
Jeffrey Liu <zhifeng172@163.com>
2022-08-04 15:43:48 +0800

    Support ssh operation

---
+++ core/sshx/ssh_test.go
@@ -0,0 +1,23 @@
+/*
+ * @Author: Jeffrey.Liu <zhifeng172@163.com>
+ * @Date: 2022-01-06 10:29:57
+ * @LastEditors: Jeffrey.Liu
+ * @LastEditTime: 2022-01-06 11:31:44
+ * @Description:
+ */
+package sshx
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSSH(t *testing.T) {
+	cli := NewSSHCli("10.0.2.113:22", "motech", "123456")
+	defer cli.Close()
+
+	cli.Run("pwd")
+
+	// fmt.Println(string(cli.Output("pwd")))
+	fmt.Println(string(cli.Output("ps -ef |head")))
+}
