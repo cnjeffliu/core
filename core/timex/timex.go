86a2240985d8472b1ec96d66c44bd8ed9468a1fc
Jeffrey Liu <zhifeng172@163.com>
2022-09-13 17:47:07 +0800

    update date util

--- core/timex/timex.go
+++ core/timex/timex.go
@@ -2,12 +2,21 @@
  * @Author: Jeffrey Liu <zhifeng172@163.com>
  * @Date: 2022-07-20 13:56:45
  * @LastEditors: Jeffrey Liu
- * @LastEditTime: 2022-08-26 11:43:42
+ * @LastEditTime: 2022-09-13 17:43:18
  * @Description:
  */
 package timex
 
-import "time"
+import (
+	"time"
+)
+
+const (
+	TIME_LAYOUT_SECOND      = "2006-01-02 15:04:05"
+	TIME_LAYOUT_MILLSECOND  = "2006-01-02 15:04:05.000"
+	TIME_LAYOUT_MICROSECOND = "2006-01-02 15:04:05.000000"
+	TIME_LAYOUT_NANOSECOND  = "2006-01-02 15:04:05.000000000"
+)
 
 func NowS() int64 {
 	return time.Now().Unix()
@@ -23,4 +32,19 @@ func ElapseMS(begin time.Time) int64 {
 
 func ElapseNS(begin time.Time) int64 {
 	return time.Now().Sub(begin).Nanoseconds()
+}
+
+// input format is 2022-01-01 01:00:00
+func StrToTime(s string) time.Time {
+	d, err := time.ParseInLocation(TIME_LAYOUT_SECOND, s, time.Local)
+	if err != nil {
+		return time.Now()
+	}
+
+	return d
+}
+
+// output format is 2022-01-01 01:00:00
+func TimeToStr(s time.Time) string {
+	return s.Format(TIME_LAYOUT_SECOND)
 }
